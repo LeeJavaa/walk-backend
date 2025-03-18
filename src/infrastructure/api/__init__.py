3ca8ae9d9971cb028f5db9a1467a5eb9423cbb46
Lee Johnson <leejavaa@gmail.com>
2025-03-18 18:26:01 +0200

    Remove API

--- src/infrastructure/api/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-API package for the Walk agentic coding system.
-
-This package implements a FastAPI-based REST API that exposes the functionality
-of the Walk system for use by frontend applications.
-"""
--- src/infrastructure/api/models/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Models package for the Walk API.
-"""
